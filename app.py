4fe03c20d918a38e691fa2dfa05faa075391ebff
Lars <larsmcordes@gmail.com>
2023-05-02 15:38:42 +0200

    added games played chart

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 import streamlit as st
+import data_analysis as da
+import altair as alt
 
 
 actually_tab, teams_tab = st.tabs(["Team Statistics", "Team Comparisons"])
 
+### Load data 
+actually_df, teams_df = da.load_data()
 
 ### Only AIFP stats ###
 with actually_tab:
-    st.write("Within team stats TEST")
+    st.title("Actually in First Place Statistics 2023")
+    n_games, first_date, last_date = da.get_games_range(actually_df)
+    st.write(f"Based on {n_games} games from {first_date} to {last_date}")
 
+    
+    #col1, col2 = st.columns(2)
 
+    #with col1:
+    
+    # Number of games per player bar chart
+    player_dat = da.get_games_per_player(actually_df)
+    games_per_player_chart =alt.Chart(player_dat).mark_bar().encode(
+        alt.X('Players:N', sort='-y'),
+        alt.Y('Games played:Q'))
+    st.altair_chart(games_per_player_chart)
+    #with col2:
+    
 
 ### Stats with other teams ###
 with teams_tab:
-    st.write("Overall team stats")
+    st.header("Overall team stats")
--- data_analysis.py
+++ data_analysis.py
@@ -1,12 +1,13 @@
 import pandas as pd
-
+import numpy as np
+from collections import Counter
 
 SHEET_ID = "13Vj450UMswqv0ycWIg3ZMTW42QNdYNDJpkAEKTvrh5Y"
 URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=xlsx"
 
 
 
-def get_data():
+def load_data():
     sheets = pd.ExcelFile(URL)
     teams_df = pd.read_excel(sheets, sheet_name="dsFact_Scores")
     actually_df = pd.read_excel(sheets, sheet_name="dsFact_Teamstats")
@@ -21,15 +22,21 @@ def add_weekday_col(df):
     df["Weekday"] = df["Weekday"].replace([2,6], ["Wednesday", "Sunday"])
     return df
 
-s
 
-ubset = actually_df.filter(regex="^Player\d$")
-subset.squeeze(axis=0)
-print(subset)
+def get_games_range(df):
+    return (len(df["Date"]),
+        df["Date"].min().strftime('%d/%m/%Y'),
+        df["Date"].max().strftime('%d/%m/%Y')
+    )
 
-actually_df["Points"].plot.density()
-#actually_df.plot.line(x="Date", y="Points")
+def get_games_per_player(df):
+    player_cols = df.filter(regex="^Player\d$")
+    game_counts = Counter(player_cols.values.flatten().tolist())
+    del game_counts[np.nan]
+    return pd.DataFrame({"Players": game_counts.keys(), "Games played": game_counts.values()})
 
 
-print(actually_df[actually_df["Weekday"]=="Wednesday"]["Points"].mean())
-print(actually_df[actually_df["Weekday"]=="Sunday"]["Points"].mean())
+# actually_df["Points"].plot.density()
+# actually_df.plot.line(x="Date", y="Points")
+# print(actually_df[actually_df["Weekday"]=="Wednesday"]["Points"].mean())
+# print(actually_df[actually_df["Weekday"]=="Sunday"]["Points"].mean())
